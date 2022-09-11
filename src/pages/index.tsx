978a6e1c737f1b27b4b75254ab84fe073fd751d2
Warlike <warlikerichard@hotmail.com>
2022-09-10 22:33:46 -0300

    test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,6 +8,12 @@ import { api } from '../services/api';
 import { Loading } from '../components/Loading';
 import { Error } from '../components/Error';
 
+interface Data{
+  pages: {
+    title, description, url, id: string;
+    ts: number;
+  }[]
+}
 export default function Home(): JSX.Element {
   const {
     data,
@@ -19,25 +25,42 @@ export default function Home(): JSX.Element {
   } = useInfiniteQuery(
     'images',
     // TODO AXIOS REQUEST WITH PARAM
+    ({pageParam = null}) => {
+      const data = api.get('/api/images', {params: {after: pageParam}});
+      return data;
+    }
     ,
     // TODO GET AND RETURN NEXT PAGE PARAM
+    {
+      getNextPageParam: (lastRequestResult) => {
+        const {data} = lastRequestResult;
+        return data.after? data.after : null;
+      }
+    }
+
   );
 
   const formattedData = useMemo(() => {
-    // TODO FORMAT AND FLAT DATA ARRAY
+    console.log(data)
+    return data
+  
   }, [data]);
 
-  // TODO RENDER LOADING SCREEN
-
-  // TODO RENDER ERROR SCREEN
-
-  return (
+  return isLoading ? <Loading/>
+  : isError ? <Error/>
+  : 
+  (
     <>
       <Header />
 
       <Box maxW={1120} px={20} mx="auto" my={20}>
         <CardList cards={formattedData} />
-        {/* TODO RENDER LOAD MORE BUTTON IF DATA HAS NEXT PAGE */}
+        {hasNextPage && <Button 
+        isLoading={isFetchingNextPage} 
+        loadingText="Carregando..." 
+        onClick={()=>{
+          fetchNextPage();
+        }}>Carregar mais</Button>}
       </Box>
     </>
   );
